51bef16f1199471cc4da2b2ed1a65e645084445d
Waleed Meselhy <waleed_meselhy@eng.asu.edu.eg>
2019-09-20 22:16:41 +0200

    initial commit

---
+++ scrap_github/scrap_github/settings.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrap_github project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'scrap_github'
+
+SPIDER_MODULES = ['scrap_github.spiders']
+NEWSPIDER_MODULE = 'scrap_github.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrap_github (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'scrap_github.middlewares.ScrapGithubSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    'scrap_github.middlewares.SleepRetryMiddleware': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+    'scrap_github.middlewares.UserAgentRotatorMiddleware': 400,
+}
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'scrap_github.pipelines.ScrapGithubPipeline': 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+AUTOTHROTTLE_START_DELAY = 10
+# The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 120
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG = True
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+RETRY_TIMES = 5
+USER_AGENT_CHOICES = [
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140205 Firefox/24.0 Iceweasel/24.3.0',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+]
---
+++ scrap_github/scrap_github/spiders/repo_used_by.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RepoUsedBySpider(scrapy.Spider):
+    name = 'repo_used_by'
+    github_url = 'https://github.com'
+    repo_name = ''
+
+    def start_requests(self):
+        used_by_url = f'{self.github_url}/{self.repo_name}/network/dependents'
+
+        yield scrapy.Request(
+            url=used_by_url,
+            callback=self.parse_search_response,
+        )
+
+    def parse_search_response(
+            self, response: scrapy.http.response.html.HtmlResponse):
+        repo: scrapy.selector.unified.Selector
+        for repo in response.selector.xpath(
+                '//span[@data-repository-hovercards-enabled]/parent::node()'):
+            repo_name = repo.xpath(
+                './/a[@data-hovercard-type="repository"]/text()'
+            ).extract_first()
+
+            repo_url = repo.xpath(
+                './/a[@data-hovercard-type="repository"]/@href').extract_first(
+                )
+            stars = repo.xpath(
+                './/*[@class="octicon octicon-star"]/parent::node()/text()'
+            ).extract()[1].replace(' ', '').replace('\n', '')
+
+            forked = repo.xpath(
+                './/*[@class="octicon octicon-repo-forked"]/parent::node()/text()'
+            ).extract()[1].replace(' ', '').replace('\n', '')
+            yield {
+                'repo_name': repo_name,
+                'stars': stars,
+                'forked': forked,
+                'repo_url': f'{self.github_url}/{repo_url}'
+            }
+        next_button_url = response.selector.css(
+            '.BtnGroup-item+ .BtnGroup-item').xpath('@href').extract_first()
+        yield scrapy.Request(
+            url=next_button_url,
+            callback=self.parse_search_response,
+        )
